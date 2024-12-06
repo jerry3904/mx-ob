8ffbdd99d88538ca6df630a9146decb025643f24
jerry3904 <stormyjerry@gmail.com>
2024-12-06 11:17:20 -0500

    corrected translating code, added README

--- tools/menu_translator.py
+++ tools/menu_translator.py
@@ -5,15 +5,24 @@ root = ET.parse("menu.xml").getroot()
 ns = {'': 'http://openbox.org/', 'xsi': 'http://www.w3.org/2001/XMLSchema-instance'}
 ET.register_namespace('', "http://openbox.org/")
 
-# csv English, German
+lang = ""
+# csv English, Lang
 with open("translate.csv", "r") as f:
     csvFile =csv.reader(f)
+    header_row = next(csvFile)
+    # get lang from cell B1
+    lang = header_row[1]
+    print(lang)
     for lines in csvFile:
         print(lines[0], lines[1])
-
         for item in root.findall('.//{*}menu', ns):
             for m in item.iter():
                 if m.tag == "{http://openbox.org/}item":
+                    if m.get('label') == lines[0]:
+                        m.set("label",lines[1].lstrip())
+                    for menu in root.findall('.//{*}menu', ns):
+		for m in menu.iter():
+                if m.tag == "{http://openbox.org/}menu":
                     if m.get('label') == lines[0]:
                         m.set("label",lines[1].lstrip())
 
@@ -21,6 +30,6 @@ with open("translate.csv", "r") as f:
 print()
 tree = ET.ElementTree(root)
 ET.indent(tree, space="  ")
-tree.write("menu_de.xml", xml_declaration=True, encoding="utf-8")
+tree.write(f"../menu-translations/menu_{lang}.xml", xml_declaration=True, encoding="utf-8")
 
 ET.dump(root)
